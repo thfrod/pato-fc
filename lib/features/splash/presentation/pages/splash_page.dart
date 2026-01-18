3bfcb274759e381fc74aae56062a2089c2ff19b2
Thiago Ferreira Rodrigues <thiago.ferreirarod22@gmail.com>
2026-01-18 15:44:34 -0300

    feat(splash): add better animation on splash screen and routing

--- lib/features/splash/presentation/pages/splash_page.dart
+++ lib/features/splash/presentation/pages/splash_page.dart
@@ -1,5 +1,9 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
+// ignore: depend_on_referenced_packages
+import 'package:page_transition/page_transition.dart';
+import 'package:patofc/features/auth/presentation/pages/login_page.dart';
 
 class SplashPage extends StatelessWidget {
   const SplashPage({super.key});
@@ -9,15 +13,13 @@ class SplashPage extends StatelessWidget {
     final colors = Theme.of(context).colorScheme;
     final animationPath = 'assets/animations/logo-animation.json';
 
-    return Scaffold(
+    return AnimatedSplashScreen(
       backgroundColor: colors.primary,
-      body: Center(
-        child: LottieBuilder.asset(
-          animationPath,
-          repeat: true,
-          width: MediaQuery.sizeOf(context).width * 0.75,
-        ),
-      ),
+      splash: Center(child: LottieBuilder.asset(animationPath, repeat: true)),
+      nextScreen: const LoginPage(),
+      splashIconSize: MediaQuery.sizeOf(context).width * 0.75,
+      splashTransition: SplashTransition.fadeTransition,
+      pageTransitionType: PageTransitionType.fade,
     );
   }
 }
